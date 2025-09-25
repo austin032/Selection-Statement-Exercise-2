afcae5f6f7061d99d405183ba6e9fffd95f2a0ad
Austin Okyere <okyereaustin@gmail.com>
2025-09-25 15:28:17 -0600

    Completed SelectionStatementsExercise2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            string favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("I don't do well with numbers");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is not my favorite school subject");
+                    break;
+                case "history":
+                    Console.WriteLine("History is not my favorite school subject");
+                    break;
+                case "english":
+                    Console.WriteLine("I don't like writing essays");
+                    break;
+                case "gym":
+                    Console.WriteLine("Gym is my favorite school subject because I like to exercise");
+                    break;
+                default:
+                    Console.WriteLine("I enjoy that subject as well");
+                    break;
+            }
         }
     }
 }
